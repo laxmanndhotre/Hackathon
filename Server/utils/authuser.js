485b88cc6ccdb9c6761bffb2543b0dd2be323414
Govind <govindju@gmail.com>
2025-11-17 18:35:27 +0530

    server bugs fixed 2

--- Server/utils/authuser.js
+++ Server/utils/authuser.js
@@ -5,25 +5,24 @@ const config = require('./config')
 function authorizeUser(req, res, next) {
 
     const url = req.url
+    console.log(url)
     if (url == '/user/signin' || url == '/user/signup' || url == '/category') 
         next()
-    else if (url == '/blog' && req.method == 'GET')
+    else if (url == '/blogs' && req.method == 'GET')
         next()
     else {
         const token = req.headers.token
         if (token) {
             try {
-                console.log("test1")
                 const payload = jwt.verify(token, config.SECRET)
                 req.headers.uid = payload.uid
                 next()
             } catch (ex) {
-                
-                console.log("test2")
                 res.send(result.createResult('Invalid Token'))
             }
-        } else
+        } else{
             res.send(result.createResult('Token is Missing'))
+        }
     }
 }
 
